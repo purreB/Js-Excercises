54d460da766f676ee64496e8b7ad2c7344c0fb8a
purreB <jakob.bergstrom@live.se>
2021-02-18 15:34:01 +0100

    Finished objects assignment

--- objects/app.js
+++ objects/app.js
@@ -1,13 +1,15 @@
-
 /* ---- Don't change anything between here --- */
 
 const teacher = {
-    name: 'Max',
-    age: 31,
-    boring: 'Sometimes',
-    fun: true
+  name: "Max",
+  age: 31,
+  boring: "Sometimes",
+  fun: true,
 };
 
 /* --- And here --- */
 
 // Print out to document, on a new line, the key / value pairs of the object.
+for (const key in teacher) {
+  document.body.innerHTML += `${key}: ${teacher[key]}<br>`;
+}
